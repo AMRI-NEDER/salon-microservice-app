266b7d19c67a001d8591968a8ff58b4551de52fe
Amri Neder <elamrineder100@gmail.com>
2026-05-01 22:25:51 +0100

    initial commit

--- user-service/Database/seed/booking-seed.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO bookings (user_id, user_name, user_email, service, date, time, status, notes)
-VALUES
-(gen_random_uuid(), 'houcem aouled marzouk', 'houcem@gmail.com', 'Classic Haircut', CURRENT_DATE, '10:00', 'confirmed', ''),
-(gen_random_uuid(), 'Jalel Amri', 'jalelamri@gmail.com', 'Beard Trim', CURRENT_DATE + 1, '11:30', 'confirmed', ''),
-(gen_random_uuid(), 'abdrahim', 'bahrini@gmail.com', 'Hair Wash', CURRENT_DATE + 2, '14:00', 'completed', ''),
-(gen_random_uuid(), 'Emma', 'emnasghiri@gmail.com', 'Skin Fade', CURRENT_DATE + 3, '16:00', 'no-show', ''),
-(gen_random_uuid(), 'Admin', 'admin@gmail.com', 'Full Grooming', CURRENT_DATE + 4, '18:00', 'confirmed', '');
--- user-service/auth-service/seed.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO users (name, email, password, role) VALUES
-(
-  'Admin',
-  'elamrineder100@gmail.com',
-  '$2b$10$scMif8MqCa5zxP.h4bjuw.78D6K1ztxZCC9MUasJsFpuF1Dp4HiUO',
-  'admin'
-)
-ON CONFLICT (email) DO NOTHING;
